565ccd30f2a085b8c5b34c0be3ddf27cba4b862d
Tatiana <tatiana.allery@orange.fr>
2025-03-05 11:51:44 +0100

    feat : creation website calling API training

---
+++ src/main/java/com/HRApplication/webapp/CustomProperties.java
@@ -0,0 +1,15 @@
+package com.HRApplication.webapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix="com.HRApplication.webapp")
+@Data
+public class CustomProperties {
+	
+	private String apiUrl;
+
+}
